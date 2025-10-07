469ef08dc9cda750207b8bf54fa0908666f29574
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2025-10-07 08:33:52 +0000

    修改bug   ImportError: cannot import name 'LlavaLlamaModel' from 'llava.model' (/home/h                   unterj/gys/VidTokenPrune/LLaVA-NeXT/llava/model/__init__.py)

--- LLaVA-NeXT/llava/model/__init__.py
+++ LLaVA-NeXT/llava/model/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 AVAILABLE_MODELS = {
-    "llava_llama": "LlavaLlamaForCausalLM, LlavaConfig",
+    "llava_llama": "LlavaLlamaForCausalLM, LlavaConfig, LlavaLlamaModel",
     "llava_qwen": "LlavaQwenForCausalLM, LlavaQwenConfig",
     "llava_mistral": "LlavaMistralForCausalLM, LlavaMistralConfig",
     "llava_mixtral": "LlavaMixtralForCausalLM, LlavaMixtralConfig",
@@ -14,3 +14,4 @@ for model_name, model_classes in AVAILABLE_MODELS.items():
         exec(f"from .language_model.{model_name} import {model_classes}")
     except Exception as e:
         print(f"Failed to import {model_name} from llava.language_model.{model_name}. Error: {e}")
+
