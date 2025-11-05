eb1373953274a65dd4ec17763f161971855f41d9
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2025-11-05 15:18:03 +0000

    添加eval.sh

---
+++ eval.sh
@@ -0,0 +1,14 @@
+# nextqa_mc_test,nextqa_oe_test,mvbench,videomme,videomme_w_subtitle,egoschema_subset
+
+# ==================================== llava video 7b
+HF_ENDPOINT=https://hf-mirror.com \
+accelerate launch --num_processes=8 \
+--main_process_port=25005 \
+-m lmms_eval \
+--model llava_onevision \
+--model_args pretrained=/data/gys/models/LLaVA-Video-7B-Qwen2,conv_template=qwen_1_5,max_frames_num=64 \
+--tasks mvbench \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix llava_onevision_float16 \
+--output_path ./logs_retain_10_percent/llava_onevision_hieravid_float16
