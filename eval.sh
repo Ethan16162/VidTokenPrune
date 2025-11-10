48b60362219d1b5c266aebb4c2850b0ddc690667
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2025-11-10 10:47:15 +0000

    eval.sh

--- eval.sh
+++ eval.sh
@@ -19,9 +19,9 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 accelerate launch --num_processes=8 \
 --main_process_port=25003 \
 -m lmms_eval \
---model llava_onevision \
---model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32 \
---tasks videomme,videomme_w_subtitle \
+--model llava_vid \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32,overwrite=False,force_sample=True,add_time_instruction=True \
+--tasks videomme_w_subtitle \
 --batch_size 1 \
 --log_samples \
 --log_samples_suffix llava_onevision_float16 \
