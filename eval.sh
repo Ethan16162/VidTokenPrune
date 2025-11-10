c39d3d4b7bec2e5262d01936161cb72391710a2f
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2025-11-10 10:04:52 +0000

    eval.sh

--- eval.sh
+++ eval.sh
@@ -15,14 +15,14 @@
 
 # ==================================== llava onevision 7b
 HF_ENDPOINT=https://hf-mirror.com \
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
-accelerate launch --num_processes=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+accelerate launch --num_processes=8 \
 --main_process_port=25003 \
 -m lmms_eval \
 --model llava_onevision \
 --model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32 \
---tasks nextqa_mc_test,nextqa_oe_test,mvbench,videomme,videomme_w_subtitle,egoschema_subset \
+--tasks videomme_w_subtitle \
 --batch_size 1 \
 --log_samples \
 --log_samples_suffix llava_onevision_float16 \
---output_path ./logs_retain_10_percent/llava_onevision_hieravid_float16_frame32
+--output_path ./logs_retain_30_percent/llava_onevision_hieravid_float16_frame32
