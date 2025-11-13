ae4abd9a3be0826a308d70ec11d69e0d85850446
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2025-11-13 11:53:58 +0000

    添加qwen2-vl

--- eval.sh
+++ eval.sh
@@ -14,15 +14,56 @@
 # --output_path ./logs_retain_30_percent/llava_video_hieravid_float16
 
 # ==================================== llava onevision 7b
+# HF_ENDPOINT=https://hf-mirror.com \
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# accelerate launch --num_processes=8 \
+# --main_process_port=25005 \
+# -m lmms_eval \
+# --model llava_vid \
+# --model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32,overwrite=False,force_sample=True,add_time_instruction=True \
+# --tasks egoschema_subset \
+# --batch_size 1 \
+# --log_samples \
+# --log_samples_suffix llava_onevision_float16 \
+# --output_path ./logs_retain_10_percent/llava_onevision_hieravid_float16_frame32
+
+# # ==================================== llava onevision 7b - original model
+# # HF_ENDPOINT=https://hf-mirror.com \
+# # CUDA_VISIBLE_DEVICES=0,1,2,3 \
+# # accelerate launch --num_processes=4 \
+# # --main_process_port=25003 \
+# # -m lmms_eval \
+# # --model llava_vid \
+# # --model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32,overwrite=False,force_sample=True,add_time_instruction=True \
+# # --tasks nextqa_oe_test,mvbench,videomme,videomme_w_subtitle \
+# # --batch_size 1 \
+# # --log_samples \
+# # --log_samples_suffix llava_onevision_float16 \
+# # --output_path ./logs_retain_100_percent/llava_onevision_original_float16_frame32
+
+# =======================================
+# HF_ENDPOINT=https://hf-mirror.com \
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# accelerate launch --num_processes=8 \
+# --main_process_port=25005 \
+# -m lmms_eval \
+# --model llava_vid \
+# --model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32,overwrite=False,force_sample=True,add_time_instruction=True \
+# --tasks mvbench \
+# --batch_size 1 \
+# --log_samples \
+# --log_samples_suffix llava_onevision_float16 \
+# --output_path ./logs_retain_10_percent/llava_onevision_framefusion_float16_frame32
+
+# ====================================== Qwen2 VL
+# Run and exactly reproduce qwen2vl results!
+# mme as an example
+# pip3 install qwen_vl_utils
 HF_ENDPOINT=https://hf-mirror.com \
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-accelerate launch --num_processes=8 \
---main_process_port=25003 \
--m lmms_eval \
---model llava_onevision \
---model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32 \
---tasks nextqa_mc_test,nextqa_oe_test,mvbench,videomme,videomme_w_subtitle,egoschema_subset \
---batch_size 1 \
---log_samples \
---log_samples_suffix llava_onevision_float16 \
---output_path ./logs_retain_10_percent/llava_onevision_framefusion_float16_frame32
+accelerate launch --num_processes=8 --main_process_port=12345 -m lmms_eval \
+    --model qwen2_vl \
+    --model_args=pretrained=Qwen/Qwen2-VL-7B-Instruct,max_pixels=2359296 \
+    --tasks mvbench \
+    --batch_size 1 --log_samples \
+    --log_samples_suffix qwen2-vl \
+    --output_path ./logs_qwen2-vl/hieravid
