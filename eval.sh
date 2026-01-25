36e78385ffea92b499c3a57da5955e1936e93e9a
Ethan16162 <guoyansong@stu.xmu.edu.cn>
2026-01-25 18:44:15 +0800

    eval.sh

--- eval.sh
+++ eval.sh
@@ -2,31 +2,32 @@
 
 # ==================================== llava video 7b
 HF_ENDPOINT=https://hf-mirror.com \
-CUDA_VISIBLE_DEVICES=7 \
-accelerate launch --num_processes=1 \
---main_process_port=25005 \
--m lmms_eval \
---model llava_vid \
---model_args pretrained=/data/gys/models/LLaVA-Video-7B-Qwen2,conv_template=qwen_1_5,max_frames_num=256 \
---tasks videomme \
---batch_size 1 \
---log_samples \
---log_samples_suffix llava_video_float16 \
---output_path ./logs_retain_20_percent/llava_video_hieravid_float16_FastDDP/
-
-# ==================================== llava onevision 7b
-# HF_ENDPOINT=https://hf-mirror.com \
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# CUDA_VISIBLE_DEVICES=7 \
 # accelerate launch --num_processes=1 \
 # --main_process_port=25005 \
 # -m lmms_eval \
 # --model llava_vid \
-# --model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=256,overwrite=False,force_sample=True,add_time_instruction=True \
+# --model_args pretrained=/data/gys/models/LLaVA-Video-7B-Qwen2,conv_template=qwen_1_5,max_frames_num=32 \
 # --tasks videomme \
 # --batch_size 1 \
 # --log_samples \
-# --log_samples_suffix llava_onevision_float16 \
-# --output_path ./logs_retain_10_percent/llava_onevision_hieravid_float16_frame32
+# --log_samples_suffix llava_video_float16 \
+# --output_path ./logs_retain_20_percent/llava_video_hieravid_float16_FastDDP/
+
+# ==================================== llava onevision 7b
+# ,overwrite=False,force_sample=True,add_time_instruction=True
+HF_ENDPOINT=https://hf-mirror.com \
+CUDA_VISIBLE_DEVICES=7 \
+accelerate launch --num_processes=1 \
+--main_process_port=25005 \
+-m lmms_eval \
+--model llava_onevision \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,max_frames_num=32 \
+--tasks videomme \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix llava_onevision_float16 \
+--output_path ./logs_retain_10_percent/llava_onevision_hieravid_float16_frame32
 
 # ==================================== llava onevision 7b - original model
 # HF_ENDPOINT=https://hf-mirror.com \
